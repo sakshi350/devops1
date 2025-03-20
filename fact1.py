1cecb83fa5f826573b1f9fca0fee2ccc10542008
sakshi350 <sakshibambade02@gmail.com>
2025-03-20 14:52:53 +0530

    Update fact1.py

--- fact1.py
+++ fact1.py
@@ -4,3 +4,4 @@ for  i in range(1,num+1):
      factorial *=i
 print(f"the factorial of {num} is {factorial} ")     
 print(f"the factorial of {num} is {factorial} ") 
+print(f"the factorial of {num} is {factorial} ") 
